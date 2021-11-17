d8db65da116e9fcb258f7890e82a7ee2d47ce8e7
AlaynaCarlin <alaynacarlin18@gmail.com>
2021-11-17 09:50:10 -0500

    login

--- src/auth/Login.js
+++ src/auth/Login.js
@@ -5,17 +5,32 @@ const Login = (props) => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
 
+    let handleSubmit = (event) => {
+        event.preventDefault();
+        fetch("http://localhost:3000/user/login", {// sending a fetch to the endpoint determined by our server
+            method: 'POST', // the method of the fetch is a POST
+            body: JSON.stringify({ user: { username: username, password: password } }), // including a body with our state  info. This has to match what the backend is expecting
+            headers: new Headers({
+                'Content-Type': 'application/json' // lets the server know what type of information we are sending it
+            })
+        }).then(
+            (response) => response.json() // jsonify the response
+        ).then((data) => {
+            props.updateToken(data.sessionToken) // sending the sessionToken to the updateToken function
+        })
+    }
+
     return(
         <div>
             <h1>Login</h1>
-            <Form>
+            <Form onSubmit={handleSubmit}>
                 <FormGroup>
                     <Label htmlFor="username">Username</Label>
-                    <Input name="username" value={username}/>
+                    <Input onChange={(e) => setUsername(e.target.value)} name="username" value={username}/>
                 </FormGroup>
                 <FormGroup>
                     <Label htmlFor="password">Password</Label>
-                    <Input name="password" value={password}/>
+                    <Input onChange={(e) => setPassword(e.target.value)} name="password" value={password}/>
                 </FormGroup>
                 <Button type="submit">Login</Button>
             </Form>
